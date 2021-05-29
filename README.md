ab1e9bbc199c548b799ad8458d75de519300b7ba
Mohamed Doffer Mohamed Nasir <73598690+mohamed-nasir@users.noreply.github.com>
2021-05-30 00:21:31 +0800

    v0.0.7

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# uikit
+# 🥞 MatrixDefi UIkit
+
+[![Version](https://img.shields.io/npm/v/@matrixpilldefi/uikit)](https://www.npmjs.com/package/@matrixpilldefi/uikit) [![Size](https://img.shields.io/bundlephobia/min/@matrixpilldefi/uikit)](https://www.npmjs.com/package/@matrixpilldefi/uikit)
+
+MatrixDefi UIkit is a set of React components and hooks used to build pages on MatrixDefi's apps. It also contains a theme file for dark and light mode.
+
+## Install
+
+`yarn add @matrixpilldefi/uikit`
+
+## Setup
+
+### Theme
+
+Before using MatrixDefi UIkit, you need to provide the theme file to styled-component.
+
+```
+import { ThemeProvider } from 'styled-components'
+import { light, dark } from '@matrixpilldefi/uikit'
+...
+<ThemeProvider theme={isDark}>...</ThemeProvider>
+```
+
+### Reset
+
+A reset CSS is available as a global styled component.
+
+```
+import { ResetCSS } from '@matrixpilldefi/uikit'
+...
+<ResetCSS />
+```
---
+++ src/theme/colors.ts
@@ -0,0 +1,126 @@
+import { Colors } from "./types";
+
+// export const baseColors = {
+//   failure: "#e02f58",
+//   primary: "#16256b",
+//   primaryBright: "#dd831c",
+//   primaryDark: "#c25e00",
+//   secondary: "#d6931a", //
+//   success: "#dace31",
+//   warning: "#e68327",
+// };
+
+// export const brandColors = {
+//   binance: "#F0B90B",
+// };
+
+// export const lightColors: Colors = {
+//   ...baseColors,
+//   ...brandColors,
+//   background: "#ffffff",
+//   secondary: "#16256b", //
+//   backgroundDisabled: "#E9EAEB",
+//   contrast: "#191326",
+//   invertedContrast: "#FFFFFF",
+//   input: "#eeeaf4",
+//   inputSecondary: "#d7caec",
+//   tertiary: "#EFF4F5",
+//   text: "#487CFF", //
+//   textDisabled: "#BDC2C4",
+//   textSubtle: "#16256b",
+//   borderColor: "#E9EAEB",
+//   card: "#FFFFFF",
+//   nav: "#16256b", //
+//   gradients: {
+//     bubblegum: "linear-gradient(139.73deg, #E6FDFF 0%, #F3EFFF 100%)",
+//   },
+// };
+
+// export const darkColors: Colors = {
+//   ...baseColors,
+//   ...brandColors,
+//   primary: "#50D6FF",
+//   secondary: "#50D6FF", //  
+//   background: "#1f2a5c", //
+//   backgroundDisabled: "#424037", //
+//   contrast: "#FFFFFF",
+//   invertedContrast: "#262513",
+//   input: "#483F5A",
+//   inputSecondary: "#8d8857",
+//   primaryDark: "#8f8104",
+//   tertiary: "#28444A", //
+//   text: "#fcfae2", //
+//   textDisabled: "#717161", //
+//   textSubtle: "#50D6FF", //
+//   borderColor: "#61634b",
+//   card: "#131a42", //
+//   nav: "#131a42", //
+//   gradients: {
+//     bubblegum: "linear-gradient(139.73deg, #313D5C 0%, #3D2A54 100%)", //
+//   },
+// };
+
+
+
+
+export const baseColors = {
+  failure: "#F71414",
+  primary: "#787878",
+  primaryBright: "#60E600",
+  primaryDark: "#5BD900",
+  secondary: "#232343",
+  success: "#68CF29",
+  warning: "#FFAB2D",
+  };
+  
+  export const brandColors = {
+    binance: "#F0B90B",
+  };
+  
+  export const lightColors: Colors = {
+    ...baseColors,
+    ...brandColors,
+    background: "#FAF9FA",
+    backgroundDisabled: "#E9EAEB",
+    contrast: "#191326",
+    invertedContrast: "#FFFFFF",
+    input: "#eeeaf4",
+    // inputBorder: "#cccccc",
+    inputSecondary: "#cccccc",
+    tertiary: "#EFF4F5",
+    text: "#452A7A",
+    textDisabled: "#BDC2C4",
+    textSubtle: "#1a8941",
+    activeButton: "#397f49",
+    borderColor: "#E9EAEB",
+    card: "#FFFFFF",
+    nav: "#FFFFFF",
+    gradients: {
+      bubblegum: "linear-gradient(139.73deg, #E6FDFF 0%, #F3EFFF 100%)",
+    },
+  };
+  
+  export const darkColors: Colors = {
+    ...baseColors,
+    ...brandColors,
+    secondary: "#e9f1fd",
+    background: "#0e0e1b",
+    backgroundDisabled: "#132a18",
+    contrast: "#FFFFFF",
+    invertedContrast: "#0d1c10",
+    input: "#787878",
+    // inputBorder: "#cccccc",
+    inputSecondary: "#cccccc",
+    primaryDark: "#80ce87",
+    tertiary: "#2d2f37",
+    text: "#FFFFFF",
+    textDisabled: "#397f49",
+    textSubtle: "#ffffff",
+    activeButton: "#397f49",
+    borderColor: "#0e0e1b",
+    card: "#204829",
+    nav: "#204829",
+    gradients: {
+      bubblegum: "linear-gradient(139.73deg, #193821 0%, #151538 100%)",
+    },
+  };
